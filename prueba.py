87d594a3b2b286ea87f7a318bf134637fbdce6c8
Marcelo Oviedo <oviedommarcelo@gmail.com>
2025-03-27 10:36:02 -0300

    add comment

--- prueba.py
+++ prueba.py
@@ -1,6 +1,7 @@
-#Se define variable nombre
-nombre = input("Bienvenido ingresa tu nombre")
-apellido = input("Bienvenido ingresa tu apellido")
-#Se imprime
-print(f"Hola {nombre} {apellido}, bienvenido.")
-print("Adios")
+#prueba de librería
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
